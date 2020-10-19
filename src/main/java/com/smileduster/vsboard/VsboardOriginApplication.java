1a4082d4c56e2827e31c9000c92a93266fc18258
SmileDuster <56777613+SmileDuster@users.noreply.github.com>
2020-10-19 20:10:48 +0800

    init

---
+++ src/main/java/com/smileduster/vsboard/VsboardOriginApplication.java
@@ -0,0 +1,13 @@
+package com.smileduster.vsboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsboardOriginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VsboardOriginApplication.class, args);
+    }
+
+}
